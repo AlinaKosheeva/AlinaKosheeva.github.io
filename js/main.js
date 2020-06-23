dc39f5751672befa7491312285ed4b6255c94973
AlinaKosheeva <67323365+AlinaKosheeva@users.noreply.github.com>
2020-06-23 20:23:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -40,7 +40,84 @@ $(document).ready(function () {
 
 	$('.porfolio-slider').slick({
 		slidesToShow: 3,
- 		slidesToScroll: 1,
+ 		slidesToScroll: 3,
   		dots: true,
 	});
 });
+
+		$(window).scroll(function() {
+	    $('.about').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.documents-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.passport').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("slideInLeft");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.attestat').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("slideInLeft");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.snils').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("slideInLeft");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.portfolio').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.foot').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
